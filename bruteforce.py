1853755fda9287c953e8ff5828f2b44e4a4d0e72
Jean Staffe <69088819+IceroDev@users.noreply.github.com>
2022-12-07 11:10:30 +0100

    Create bruteforce.py

---
+++ bruteforce.py
@@ -0,0 +1,21 @@
+import hashlib
+import itertools
+
+def bruteforce(passwordHash):
+
+    wordlist = "abcdefghijklmnopqrstuvwxyz"
+    y=''
+    length=1
+    wordlistHash=''
+    passwordHash=passwordHash
+
+    while wordlistHash != passwordHash:
+        for c in itertools.product(wordlist, repeat=length):
+            word = y.join(c)
+            wordlistHash = hashlib.sha3_512(word.encode("utf-8")).hexdigest()
+            if wordlistHash == passwordHash:
+                print(f"Found password: {word}")
+                break
+        length=length + 1
+
+bruteforce('99fb8667d94c08080873cdde10c5e0430baa59752d9f1204c1305176c50601fb63b438bd92b8fff633e5bd9952f7d857eb3a97d9d24871e22ad2db1e86ea95a7')
